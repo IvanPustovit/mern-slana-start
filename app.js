f23abdc2509d952efe6c69f5b74c590252118f04
IvanPustovit <pustovit.for@gmail.com>
2020-10-01 17:04:47 +0300

    Mern-slana-fix124444144

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
 app.use(function (req, res, next) {
   res.header(
     "Content-Security-Policy",
-    "default-src 'self' https://fonts.googleapis.com;style-src 'self' https://fonts.googleapis.com;  font-src 'self' data: https://fonts.gstatic.com; script-src 'self' hash ('sha256-eE1k/Cs1U0Li9/ihPPQ7jKIGDvR8fYw65VJw+txfifw=')" //;object-src 'unsafe-inline';img-src 'self';media-src 'unsafe-inline';frame-src 'unsafe-inline';font-src 'unsafe-inline';connect-src 'unsafe-inline';style-src 'unsafe-inline'; style-src-elem 'self' '*.googleapis.com'"
+    "default-src 'self' https://fonts.googleapis.com;style-src 'self' https://fonts.googleapis.com;  font-src 'self' data: https://fonts.gstatic.com; script-src 'self' hash sha256-eE1k/Cs1U0Li9/ihPPQ7jKIGDvR8fYw65VJw+txfifw=" //;object-src 'unsafe-inline';img-src 'self';media-src 'unsafe-inline';frame-src 'unsafe-inline';font-src 'unsafe-inline';connect-src 'unsafe-inline';style-src 'unsafe-inline'; style-src-elem 'self' '*.googleapis.com'"
   );
   next();
 });
